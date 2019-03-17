ccb9d2a0d59e007aa51fdd5e586950d822818ef1
GilbertXiao <gilxyj@163.com>
2019-03-18 00:23:52 +0800

    activit-ui change spring to springboot-1.2.6

---
+++ modules/activiti-ui/activiti-app/src/main/java/org.activiti.app.ui/ActivitiUIApplication.java
@@ -0,0 +1,66 @@
+package org.activiti.app.ui;
+
+import org.activiti.app.conf.ApplicationConfiguration;
+import org.activiti.app.servlet.ApiDispatcherServletConfiguration;
+import org.activiti.app.servlet.AppDispatcherServletConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * @program: ActivitiNew2019
+ * @description: springboot entrance
+ * @author: GilbertXiao
+ * @create: 2019-03-17 23:39
+ **/
+@SpringBootApplication
+@Import({ApplicationConfiguration.class})
+public class ActivitiUIApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args){
+        SpringApplication.run(ActivitiUIApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ActivitiUIApplication.class);
+    }
+
+    @Bean
+    public ServletRegistrationBean apiDispatcher(){
+        DispatcherServlet api = new DispatcherServlet();
+        api.setContextClass(AnnotationConfigWebApplicationContext.class);
+        api.setContextConfigLocation(ApiDispatcherServletConfiguration.class.getName());
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean();
+        registrationBean.setServlet(api);
+        registrationBean.addUrlMappings("/api/*");
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.setAsyncSupported(true);
+        registrationBean.setName("api");
+
+        return registrationBean;
+    }
+
+    @Bean
+    public ServletRegistrationBean appDispatcher(){
+        DispatcherServlet app = new DispatcherServlet();
+        app.setContextClass(AnnotationConfigWebApplicationContext.class);
+        app.setContextConfigLocation(AppDispatcherServletConfiguration.class.getName());
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean();
+        registrationBean.setServlet(app);
+        registrationBean.addUrlMappings("/app/*");
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.setAsyncSupported(true);
+        registrationBean.setName("app");
+
+        return registrationBean;
+    }
+}
+
