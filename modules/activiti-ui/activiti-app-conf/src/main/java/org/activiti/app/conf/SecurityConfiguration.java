ec7cca52d503389381d98c197135882c3bc71afa
GilbertXiao <gilxyj@163.com>
2019-03-25 00:56:17 +0800

    auth by druid and actuator and manage

--- modules/activiti-ui/activiti-app-conf/src/main/java/org/activiti/app/conf/SecurityConfiguration.java
+++ modules/activiti-ui/activiti-app-conf/src/main/java/org/activiti/app/conf/SecurityConfiguration.java
@@ -160,7 +160,10 @@ public class SecurityConfiguration {
 	                .antMatchers("/app/rest/idm/email-actions/*").permitAll()
 	                .antMatchers("/app/rest/idm/signups").permitAll()
 	                .antMatchers("/app/rest/idm/passwords").permitAll()
-	                .antMatchers("/app/**").authenticated();
+	                .antMatchers("/app/**").authenticated()
+					.antMatchers("/druid/**").authenticated()
+	        		.antMatchers("/actuator/**").authenticated()
+					.antMatchers("/manage/**").authenticated();
 
 	        // Custom login form configurer to allow for non-standard HTTP-methods (eg. LOCK)
 	        CustomFormLoginConfig<HttpSecurity> loginConfig = new CustomFormLoginConfig<HttpSecurity>();
